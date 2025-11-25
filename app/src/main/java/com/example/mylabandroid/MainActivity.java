6e828fac6c7676d111f10d97f5d1a9aa3a7568bc
marki <markian3838@gmail.com>
2025-11-25 09:45:12 +0200

    Implementing a Java project with multiple activities

--- app/src/main/java/com/example/mylabandroid/MainActivity.java
+++ app/src/main/java/com/example/mylabandroid/MainActivity.java
@@ -5,88 +5,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
+    String s = "відповідь прийнято";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
-    /**
-     * Збільшує рахунок першої команди на 1 очко
-     */
-    public void addOneForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 1;
-        displayForTeamA(scoreTeamA);
+    public void addPointA(View v) {
+        display(s);
     }
-
-    /**
-     * Збільшує рахунок першої команди на 2 очки
-     */
-    public void addTwoForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 2;
-        displayForTeamA(scoreTeamA);
+    public void addPointB(View v) {
+        display(s);
     }
-
-    /**
-     * Збільшує рахунок першої команди на 3 очки
-     */
-    public void addThreeForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 3;
-        displayForTeamA(scoreTeamA);
+    public void addPointC(View v) {
+        display(s);
     }
-
-    /**
-     * Збільшує рахунок другої команди на 1 очко
-     */
-    public void addOneForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 1;
-        displayForTeamB(scoreTeamB);
+    public void display(String s) {
+        TextView scoreView = findViewById(R.id.txt);
+        scoreView.setText(String.valueOf(s));
     }
-
-    /**
-     * Збільшує рахунок другої команди на 2 очки
-     */
-    public void addTwoForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 2;
-        displayForTeamB(scoreTeamB);
-    }
-
-    /**
-     * Збільшує рахунок другої команди на 3 очко
-     */
-    public void addThreeForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 3;
-        displayForTeamB(scoreTeamB);
-    }
-
-    /**
-     * Виводить на екран рахунок першої команди
-     */
-    public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
-    }
-    /**
-     * Виводить на екран рахунок другої команди
-     */
-    public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
-    }
-
-    /**
-     * Оновлення стану обох рахунків
-     */
-    public void resetScore(View v) {
-        scoreTeamA = 0;
-        scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
-    }
-
-
 }
