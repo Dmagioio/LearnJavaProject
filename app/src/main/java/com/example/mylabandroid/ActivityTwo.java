f6a31926e3f030cab1ae569300852abb8718f049
marki <markian3838@gmail.com>
2025-11-25 10:03:32 +0200

    Creating and filling the logic of the second activity

--- app/src/main/java/com/example/mylabandroid/ActivityTwo.java
+++ app/src/main/java/com/example/mylabandroid/ActivityTwo.java
@@ -6,22 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class ActivityTwo extends AppCompatActivity implements View.OnClickListener {
     String s = "відповідь прийнято";
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.btnActTwo);
+        setContentView(R.layout.activity_two);
+        btn = findViewById(R.id.btnActThree);
         btn.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if (id == R.id.btnActTwo) {
-            Intent intent = new Intent(this, ActivityTwo.class);
+        if (id == R.id.btnActThree) {
+            Intent intent = new Intent(this, ActivityThree.class);
             startActivity(intent);
         }
     }
